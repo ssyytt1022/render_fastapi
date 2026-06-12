58a251036f81575882bd11f76553baa6a41d5ec3
ssyytt1022 <suyutao1022@gmail.com>
2026-06-12 12:12:24 +0900

    add index page

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from typing import Optional
+from fastapi.responses import HTMLResponse
 
-from fastapi import FastAPI
+### コードいろいろ... ###
 
-import random  # randomライブラリを追加
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-
-    return omikuji_list[random.randrange(10)]
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
